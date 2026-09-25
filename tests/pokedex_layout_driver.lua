2a87d5cabf9e692d45791944ce3453bafc3567de
Panduino <59328953+Panduino@users.noreply.github.com>
2026-09-25 00:37:53 -0400

    Make companion mod a standalone root package

--- tests/pokedex_layout_driver.lua
+++
@@ -1,5 +0,0 @@
--- Compatibility entry point for the expanded shared-presentation QA.
-return function(game)
-  return dofile(assert(os.getenv("PC_REPO"))
-    .. "/mods/modern_ui_suite/tests/pokedex_parity_driver.lua")(game)
-end
